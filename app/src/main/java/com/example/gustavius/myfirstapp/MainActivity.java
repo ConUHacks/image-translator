07f4ea7ae9616dd07cfbd5fccd8b8322f91782e6
friesenA <amanda.friesen@gmail.com>
2017-01-21 16:23:02 -0500

    photo captured

--- app/src/main/java/com/example/gustavius/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/gustavius/myfirstapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.gustavius.myfirstapp;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.graphics.Bitmap;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import android.content.Context;
@@ -13,18 +15,57 @@ import android.content.pm.PackageManager;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    String mCurrentPhotoPath;
-    Intent takePictureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+
+    private ImageView ImageView;
+    private Bitmap ImageBitmap;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    Button.OnClickListener mTakePicOnClickListener =
+            new Button.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dispatchTakePictureIntent();
+                }
+            };
+
+    private void setBtnListenerOrDisable(
+            Button.OnClickListener onClickListener,
+            String intentName
+    ) {
+        if (isIntentAvailable(this, intentName)) {
+            btn.setOnClickListener(onClickListener);
+        } else {
+            btn.setText(
+                    getText(R.string.cannot).toString() + " " + btn.getText());
+            btn.setClickable(false);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button picBtn = (Button) findViewById(R.id.btnIntend);
+        setBtnListenerOrDisable(
+                mTakePicOnClickListener,
+                MediaStore.ACTION_IMAGE_CAPTURE
+        );
+
     }
 
-    private void captureImage() {
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            ImageBitmap = (Bitmap) extras.get("data");
+            ImageView.setImageBitmap(ImageBitmap);
         }
     }
 
