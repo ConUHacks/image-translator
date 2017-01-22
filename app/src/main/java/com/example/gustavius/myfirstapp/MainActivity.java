6832bb16d0cbb37cc70fa01e8aea50af58b52bf0
Drapha <danielisakov@yahoo.ca>
2017-01-21 21:14:15 -0500

    spinners update

--- app/src/main/java/com/example/gustavius/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/gustavius/myfirstapp/MainActivity.java
@@ -9,11 +9,13 @@ import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.graphics.Bitmap;
-import android.widget.Button;
-import android.widget.ImageView;
 
-import android.content.Context;
-import android.content.pm.PackageManager;
+import android.widget.ImageView;
+import android.widget.AdapterView;
+
+import android.app.Activity;
+import android.widget.AdapterView.OnItemSelectedListener;
+
 
 import java.io.File;
 import java.io.IOException;
@@ -23,16 +25,32 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int SELECT_PICTURE = 10;
 
     private static final String JPEG_FILE_PREFIX = "IMG_";
     private static final String JPEG_FILE_SUFFIX = ".jpg";
     private static final String CAMERA_DIR = "/dcim/";
 
-    private ImageView imageView;
-    private Bitmap imageBitmap;
+    private ImageView imageView; //displayed image back to user
+    private Bitmap imageBitmap; //send to the API
 
-    private String currentPhotoPath;
+    private String currentPhotoPath; //path where image is storred in gallery
 
+
+    /**
+     * main function do not touch
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    /**
+     * Captures photo
+     */
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
@@ -51,6 +69,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * saves image as a bitmap, returns it as a display
+     * @return
+     * @throws IOException
+     */
     private File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
@@ -60,6 +83,10 @@ public class MainActivity extends AppCompatActivity {
         return imageF;
     }
 
+    /**
+     * Creates an album directory
+     * @return
+     */
     private File getAlbumDir() {
         File storageDir = null;
 
@@ -87,24 +114,20 @@ public class MainActivity extends AppCompatActivity {
         return storageDir;
     }
 
-    private void BtnListener(){
+    /**
+     * button press listener
+     */
+    private void captureImage(){
         currentPhotoPath = null;
         dispatchTakePictureIntent();
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button picBtn = (Button) findViewById(R.id.btnIntend);
-        setBtnListenerOrDisable(
-                mTakePicOnClickListener,
-                MediaStore.ACTION_IMAGE_CAPTURE
-        );
-
-    }
-
+    /**
+     * after button press. Should extract from gallery, process and output
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
@@ -122,6 +145,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * adds pic to gallery
+     */
     private void galleryAddPic() {
         Intent mediaScanIntent = new Intent("android.intent.action.MEDIA_SCANNER_SCAN_FILE");
         File f = new File(currentPhotoPath);
@@ -130,6 +156,33 @@ public class MainActivity extends AppCompatActivity {
         this.sendBroadcast(mediaScanIntent);
     }
 
+
+    /**
+     * dynamically changes language displayed
+     */
+    public class SpinnerActivity extends Activity implements OnItemSelectedListener {
+        public void onItemSelected(AdapterView<?> parent, View view,
+                                   int pos, long id) {
+            // An item was selected. You can retrieve the selected item using
+            // parent.getItemAtPosition(pos)
+            if(parent.getItemAtPosition(pos) == 1) //spanish
+            {
+
+            }
+            else if(parent.getItemAtPosition(pos) == 0) //iroquois
+            {
+
+            }
+            else //french
+            {
+
+            }
+        }
+
+        public void onNothingSelected(AdapterView<?> parent) {
+            // Another interface callback
+        }
+    }
 
 
 }
